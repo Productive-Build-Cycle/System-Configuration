26b8570ea1131cb0ee3be3532319cc1a5c23ad05
Tara <tarashafieyan33@gmail.com>
2025-12-27 12:33:53 +0330

    #issue 1 : Created Feature Flag entity in the Domain layer and DbContext in the Infrastructure layer. Configured the connection string in appsettings.Development.json. Created the first migration and updated the SQL Server database.

---
+++ src/PBC.SystemConfiguration.Domain/Entities/FeatureFlag.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace PBC.SystemConfiguration.Domain.Entities
+{
+    public class FeatureFlag
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id {  get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.UtcNow;
+        public DateTime LastUpdateDate { get; set; } = DateTime.UtcNow;
+    }
+}
