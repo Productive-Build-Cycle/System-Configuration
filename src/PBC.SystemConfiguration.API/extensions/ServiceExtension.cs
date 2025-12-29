62a3445ce3b62bdf941dc0477b260aa62bcdd251
Hamid Bagheri <hamidbagheri9846@gmail.com>
2025-12-29 11:46:05 +0330

    feat(Repository): implement FeatureFlagRepository and AppSettingRepository and DI for repositories

---
+++ src/PBC.SystemConfiguration.API/extensions/ServiceExtension.cs
@@ -0,0 +1,14 @@
+﻿using PBC.SystemConfiguration.Infrastructure;
+
+namespace PBC.SystemConfiguration.API.Extensions;
+
+public static class ServiceExtension
+{
+    public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddInfrastructureServices();
+
+        return services;
+    }
+}
