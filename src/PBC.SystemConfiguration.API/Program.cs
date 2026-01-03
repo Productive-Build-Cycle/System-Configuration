9b809d3f0514c84dd68a0e7d4e4739c4a452ae66
Hamid Bagheri <hamidbagheri9846@gmail.com>
2026-01-03 15:46:59 +0330

    feat(swagger): add swagger package and ui in order to easy use of endpoints

--- src/PBC.SystemConfiguration.API/Program.cs
+++ src/PBC.SystemConfiguration.API/Program.cs
@@ -1,19 +1,19 @@
-using Microsoft.EntityFrameworkCore;
 using PBC.SystemConfiguration.API.Extensions;
-using PBC.SystemConfiguration.Infrastructure.Persistence.DbContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.ConfigureServices(builder.Configuration);
-
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 //Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
